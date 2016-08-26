f844cf66c96ad60ebc55d3955ca6e1d820608ea4
jalilag <agoumi.jalil@gmail.com>
2016-08-26 20:07:33 +0200

    Ajout de git

---
+++ git/utils.sh
@@ -0,0 +1,5 @@
+# Procédure de mise en place d'un dossier git
+1 : Création du dossier sur github
+2 : git init -> initialisation du dossier en local
+3 : git remote add nom_du_dossier lien -> Configuration local pour le push
+4 : git add*; git commit -m "First commit"; git push nom_du_dossier -> Envoi distant des infos
