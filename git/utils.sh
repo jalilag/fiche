7a7c9703969777965f77ba36dcdbcb1cf4b25ef2
jalil <agoumi.jalil@gmail.com>
2016-12-23 19:51:36 +0100

    init

--- git/utils.sh
+++ git/utils.sh
@@ -3,3 +3,5 @@
 2 : git init -> initialisation du dossier en local
 3 : git remote add nom_du_dossier lien -> Configuration local pour le push
 4 : git add*; git commit -m "First commit"; git push nom_du_dossier -> Envoi distant des infos
+
+sdf
