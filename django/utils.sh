7246f6ca6efa0702e6cdf0c52aa7c2dcc32b47a6
jalilag <agoumi.jalil@gmail.com>
2016-12-24 20:36:01 +0100

    Django add

--- django/utils.sh
+++ django/utils.sh
@@ -7,6 +7,6 @@ pip install Django # Installation de django (une fois l'env chargé)
 pip install Django --upgrade # Maj de django
 django-admin.py startproject project_name # Création d'un nouveau projet
 python manage.py runserver # Démarrer le serveur
-python manage.py startapp # Démarrer le serveur
+python manage.py startapp # Nouvelle application
 
 STATIC_URL #fait référence aux dossiers static dans app/static/ et n'ont pas à la racine. Utilisez STATIC_DIRS pour ajouter un dossier racine
