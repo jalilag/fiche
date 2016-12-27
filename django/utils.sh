d8c982eb7542b835da9ec722b048801ed4c39b66
jalilag <agoumi.jalil@gmail.com>
2016-12-27 16:47:27 +0100

    Ajout git

--- django/utils.sh
+++ django/utils.sh
@@ -10,3 +10,26 @@ python manage.py runserver # Démarrer le serveur
 python manage.py startapp # Nouvelle application
 
 STATIC_URL #fait référence aux dossiers static dans app/static/ et n'ont pas à la racine. Utilisez STATIC_DIRS pour ajouter un dossier racine
+
+STATIC_URL = '/static/' 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR,os.path.join(BASE_DIR,'template')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'crepes.context_processors.generic_information',
+            ],
+        },
+    },
+]
