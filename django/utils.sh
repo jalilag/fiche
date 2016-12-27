5a17dc4a980d660638de02140eb15ef323a7f5b3
jalilag <agoumi.jalil@gmail.com>
2016-12-27 19:09:18 +0100

    Django add

--- django/utils.sh
+++ django/utils.sh
@@ -9,27 +9,17 @@ django-admin.py startproject project_name # Création d'un nouveau projet
 python manage.py runserver # Démarrer le serveur
 python manage.py startapp # Nouvelle application
 
-STATIC_URL #fait référence aux dossiers static dans app/static/ et n'ont pas à la racine. Utilisez STATIC_DIRS pour ajouter un dossier racine
-
+#fait référence aux dossiers static dans app/static/ et n'ont pas à la racine. Utilisez STATIC_DIRS pour ajouter un dossier racine
+# Le dossier static contient tout les fichiers autres que le code (images, css, son ...)
+STATIC_URL 
 STATIC_URL = '/static/' 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,os.path.join(BASE_DIR,'template')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.template.context_processors.i18n',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'crepes.context_processors.generic_information',
-            ],
-        },
-    },
-]
+# Lorsque l'on doit charger un ensemble de variables générales il faut utiliser le context processors.
+# Pour cela on crée un fichier context_processors.py dans le dossier de l'appli.
+# On ajoute dedans les fonctions récupérant les variables.
+# Enfin dans la variable de TEMPLATES de settings.py on ajoute le context dans la liste context_processors tel que:
+'appname.context_processors.funcname'
+
