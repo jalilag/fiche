e34c873a9302e78ce3638904eb3421844bea0653
jalilag <agoumi.jalil@gmail.com>
2017-03-24 10:42:55 +0100

    Procédure init git repos ok

--- git/utils.sh
+++ git/utils.sh
@@ -5,7 +5,7 @@ git config --global user.name "name"
 # Procédure de mise en place d'un dossier git
 1 : Création du dossier sur github
 2 : git init -> initialisation du dossier en local
-3 : git remote add nom_du_dossier lien -> Configuration local pour le push
+3 : git remote add origin lien -> Configuration local pour le push
 4 : git add*; git commit -m "First commit"; git push nom_du_dossier -> Envoi distant des infos ... 
 
 # Procédure lors de la récupération d'un dossier git existant
